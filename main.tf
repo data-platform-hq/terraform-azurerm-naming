45a80d8ef7ae27108da563e87fb039f18fa27ed6
Myroslav Levchyk <Myroslav_Levchyk@epam.com>
2024-04-05 14:01:38 +0300

    fmt

--- main.tf
+++ main.tf
@@ -86,11 +86,11 @@ locals {
   # Purview
   purview  = substr(join("-", compact(["pvw", var.project, var.environment, var.location, var.instance_number])), 0, 63)
   purviews = { for item in var.purviews : item => substr(join("-", compact(["pvw", var.project, item, var.environment, var.location, var.instance_number])), 0, 63) }
-  
+
   # Dashboard
   dashboard  = substr(join("-", compact(["dsb", var.project, var.environment, var.location, var.instance_number])), 0, 63)
   dashboards = { for item in var.dashboards : item => substr(join("-", compact(["dsb", var.project, item, var.environment, var.location, var.instance_number])), 0, 63) }
-  
+
   # Dashboard
   workbook  = substr(join("-", compact(["wbk", var.project, var.environment, var.location, var.instance_number])), 0, 63)
   workbooks = { for item in var.workbooks : item => substr(join("-", compact(["wbk", var.project, item, var.environment, var.location, var.instance_number])), 0, 63) }
